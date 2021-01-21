7063c16e33b8b3fbd8b96467af154e48486735f1
Costantino <costantino.ventricelli@gmail.com>
2021-01-21 08:43:21 +0100

    fix type error

--- DeepLearningClassifier/RHSDistanceExtraction.py
+++ DeepLearningClassifier/RHSDistanceExtraction.py
@@ -58,8 +58,8 @@ class RHSDistanceExtract:
             id = FileManager.get_id_from_path(path)
             state = FileManager.get_state_from_id(id, control_list)
             partial_x, partial_y, partial_bs = RHSDistanceExtract.__read_samples_from_file(path)
-            partial_x = np.interp(partial_x, [np.min(partial_x), np.max(partial_y)], INTERPOL_INTERVAL)
-            partial_y = np.interp(partial_y, [np.min(partial_y), np.max(partial_y)], INTERPOL_INTERVAL)
+            partial_x = list(np.interp(partial_x, [np.min(partial_x), np.max(partial_y)], INTERPOL_INTERVAL))
+            partial_y = list(np.interp(partial_y, [np.min(partial_y), np.max(partial_y)], INTERPOL_INTERVAL))
             partial_x, partial_y, partial_bs = self.__transform_point_in_rhs(partial_x, partial_y, partial_bs)
             partial_x, partial_y, partial_bs = self.__extract_subs_from_samples(partial_x, partial_y, partial_bs)
             if state == HEALTHY_STATE:
@@ -172,7 +172,7 @@ class RHSDistanceExtract:
         # Elimino i duplicati dalle lista.
         partial_x, partial_y, timestamp, partial_bs = FileManager.delete_duplicates(partial_x,
                                                                                     partial_y, timestamp, partial_bs)
-        return partial_x, partial_y, partial_bs
+        return np.array(partial_x), np.array(partial_y), partial_bs
 
     """
         Questo metodo si occupa della trasformazione di punti campionati in sequenze rhs.
