try fix metrics calculation

Expreriment/LeaveOneOutExperiment.py
@@ -55,8 +55,8 @@ class LeaveOneOutExperiment:
             self.__ml_model = MLModel(tensor_training, training_states, tensor_validation, validation_states)
             print("Testing model...")
             with open(path.join(SAVING_PATHS, "log_file_" + str(i) + ".txt"), 'w') as file:
-                predicted_results = []
-                diagnosed_states = []
+                predicted_results = np.zeros(0)
+                diagnosed_states = np.zeros(0)
                 for test_path in test_paths:
                     id = FileManager.get_id_from_path(test_path)
                     state = FileManager.get_state_from_id(id)
@@ -65,10 +65,10 @@ class LeaveOneOutExperiment:
                     file.write("File analized: " + test_path + " state predicted for file: " + str(avg_state)
                                + " state diagnosed: " + str(state) + " for patient: " + str(id) + "\n")
                     file.write("Evaluation result => loss: " + str(evaluation_result[0]) + ", accuracy: " + str(evaluation_result[1]) + "\n")
-                    predicted_results.append(predicted_result)
-                    diagnosed_states.append(states)
-                predicted_results = np.reshape(predicted_result, 1)
-                diagnosed_states = np.reshape(diagnosed_states, 1)
+                    print("States: ", np.shape(states))
+                    print("Result: ", np.shape(predicted_result))
+                    predicted_results = np.concatenate(predicted_results, predicted_result)
+                    diagnosed_states = np.concatenate(diagnosed_states, states)
                 accuracy, precision, recall, f_score = self.__ml_model.evaluate_results(predicted_results, diagnosed_states)
                 file.write("GLOBAL ACCURACY: " + str(accuracy * 100) + "%\n")
                 file.write("GLOBAL PRECISION: " + str(precision * 100) + "%\n")