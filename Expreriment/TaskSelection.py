cd106184bd5d86d67dc95c70cf17b476ba45e832
Costantino <costantino.ventricelli@gmail.com>
2021-01-25 19:38:00 +0100

    fix file selection

--- Expreriment/TaskSelection.py
+++ Expreriment/TaskSelection.py
@@ -35,7 +35,7 @@ class TaskSelection:
         for task in self.__tasks:
             # Per il test vengono selezionati due utenti uno etichettato come sano e uno etichettato come malato, per ottenere
             # misuarizioni sulle prestazioni del sistema più precise ed affidabili.
-            paths, test_paths, validation_number = self.__select_paths(task)
+            paths, test_paths, validation_number = self.__select_paths_from_tasks([task])
             # Questo controllo evita la catastrofe in quanto permette di verificare che ci siano entrambe le classi per
             # poter effettuare la classificazione, quindi con questo controllo le possibilità di incorrere in problemi di
             # bilanciamento del dataset si riduce accorpandosi con il controllo fatto al momento dell'estrazione delle feature.
@@ -60,39 +60,60 @@ class TaskSelection:
         # La selezione continuerà fino al deterioramento dei risultati, ovvero appena uno dei quatto paramentri si abbassa
         # la selezione viene interrotta.
         while previous_max[KEY_TUPLE] <= actual_tuple[KEY_TUPLE]:
+            # Seleziono la lista dei task appartenente al migliore dei risultati selezionato precedentemente.
             actual_tasks = actual_tuple[VALUE_TUPLE]
+            # Creo il nuovo dizionario che permetterà di salvare i risultati
             best_results = {}
+            # Scansionando tutti i task dovrei essere in grado di aggiungere nuovi task alla selezione.
             for task in self.__tasks:
-                actual_tasks.append(task)
-                paths, test_paths, validation_number = self.__select_paths(actual_tasks)
-                test_states, test_tensor, training_states, training_tensor, validation_states, validation_tensor = self.__extract_rhs_segment(
-                    paths, test_paths, validation_number)
-                best_results = TaskSelection.__create_and_evaluate_model(actual_tasks, test_states, test_tensor, training_states, training_tensor,
-                                                                         validation_states, validation_tensor, best_results)
-            previous_max = deepcopy(actual_tuple)
-            actual_tuple = max(best_results.items())
+                # Verifico che il task selezionato non sia già stato preso in analisi.
+                if task not in actual_tasks:
+                    # Aggiungo il task alla lista di nuovi task da analizzare.
+                    actual_tasks.append(task)
+                    # Seleziono i file per il modello compresi i file di test e il numero di file che verranno destinati
+                    # alla validazione.
+                    paths, test_paths, validation_number = self.__select_paths_from_tasks(actual_tasks)
+                    # Estraggo i segmenti RHS per i tensori.
+                    test_states, test_tensor, training_states, training_tensor, validation_states, validation_tensor = self.__extract_rhs_segment(
+                        paths, test_paths, validation_number)
+                    # Addestro e valuto il modello
+                    best_results = TaskSelection.__create_and_evaluate_model(actual_tasks, test_states, test_tensor, training_states, training_tensor,
+                                                                             validation_states, validation_tensor, best_results)
+                # Salvo i risultati precedenti prima di aggiornare il sitema con i nuovi dati.
+                previous_max = deepcopy(actual_tuple)
+                actual_tuple = max(best_results.items())
         return previous_max
 
-    def __select_paths(self, task):
-        paths = TaskManager.get_list_from_task(task, self.__file_manager.get_files_path())
+    def __select_paths_from_tasks(self, tasks):
+        healthy_ids, disease_ids = FileManager.get_healthy_disease_list()
+        paths = []
+        for task in tasks:
+            paths += TaskManager.get_list_from_task(task, self.__file_manager.get_files_path())
         paths = FileManager.filter_file(paths, min_dim=self.__minimum_samples + 1)
-        test_paths = TaskSelection.__get_test_paths(paths)
+        healthy_paths = TaskSelection.__get_test_for_state(healthy_ids, paths)
+        disease_paths = TaskSelection.__get_test_for_state(disease_ids, paths)
+        if not healthy_paths or not disease_paths:
+            test_paths = []
+        else:
+            test_paths = healthy_paths + disease_paths
+        for i in range(len(test_paths)):
+            if test_paths[i] in paths:
+                del paths[paths.index(test_paths[i])]
         validation_number = int(np.ceil(len(paths) * 0.2))
         return paths, test_paths, validation_number
 
     @staticmethod
-    def __get_test_paths(paths):
-        test_paths = [paths[0]]
-        prev_states = FileManager.get_state_from_id(FileManager.get_id_from_path(paths[0]))
-        paths.remove(paths[0])
+    def __get_test_for_state(ids, paths):
         i = 0
-        while len(test_paths) <= 1 and i < len(paths):
-            if prev_states != FileManager.get_state_from_id(FileManager.get_id_from_path(paths[i])):
-                test_paths.append(paths[i])
-                paths.remove(paths[i])
+        return_paths = []
+        while len(return_paths) != 0 and i < len(return_paths):
+            return_paths = FileManager.get_all_file_of_id(ids[i], paths)
             i += 1
-        return test_paths
+        return return_paths
 
+    """
+    
+    """
     def __extract_rhs_segment(self, paths, test_paths, validation_number):
         training_tensor, training_states, _ = self.__feature_extraction.extract_rhs_known_state(
             paths[0: (len(paths) - validation_number)])
