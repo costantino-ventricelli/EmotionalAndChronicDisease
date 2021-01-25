38b9e4dbe9c1e52af7911a3c580012fb7cd54cc7
Costantino <costantino.ventricelli@gmail.com>
2021-01-25 16:36:45 +0100

    task selection test dependent

--- Expreriment/TaskSelection.py
+++ Expreriment/TaskSelection.py
@@ -4,44 +4,55 @@ import numpy as np
 
 from DatasetManager import FileManager
 from DatasetManager.Costants import *
-from DeepLearningClassifier.MachineLearningModel import MLModel
-from DeepLearningClassifier.RHSDistanceExtraction import RHSDistanceExtract
-from DeepLearningClassifier.TaskManager import TaskManager
+from DeepLearningClassifier import *
+from copy import deepcopy
+
+
+KEY_TUPLE = 0
+VALUE_TUPLE = 1
 
 
 class TaskSelection:
 
-    def __init__(self, samples_len, minimum_samples, validation_patients, test_patients):
+    def __init__(self, samples_len, minimum_samples):
         self.__tasks = [CLOCK, NATURAL_SENTENCE, PENTAGON, MATRIX_1, MATRIX_2, MATRIX_3, TRIAL_1, T_TRIAL_1, T_TRIAL_2,
                         TRIAL_2, HELLO, V_POINT, H_POINT, SQUARE, SIGNATURE_1, SIGNATURE_2, COPY_SPIRAL, TRACED_SPIRAL,
                         BANK_CHECK, LE, MOM, WINDOW, LISTENING]
         self.__samples_len = samples_len
         self.__minimum_samples = minimum_samples
-        self.__validation_patients = validation_patients
-        self.__test_patients = test_patients
         self.__best_results = {}
         self.__file_manager = FileManager("Dataset")
-        feature_extraction = RHSDistanceExtract(minimum_samples, samples_len)
+        self.__feature_extraction = RHSDistanceExtract(minimum_samples, samples_len)
         for task in self.__tasks:
-            paths = TaskManager.get_list_from_task(task, self.__file_manager.get_files_path())
-            paths = FileManager.filter_file(paths, min_dim=minimum_samples + 1)
-            test_paths = TaskSelection.__get_test_paths(paths)
-            validation_number = int(np.ceil(len(paths) * 0.2))
-            training_tensor, training_states, _ = feature_extraction.extract_rhs_known_state(paths[0: (len(paths) - validation_number)])
-            validation_tensor, validation_states, _ = feature_extraction.extract_rhs_known_state(paths[(len(paths) - validation_number): len(paths)])
-            test_tensor, test_states, _ = feature_extraction.extract_rhs_known_state(test_paths)
-            machine_learning = MLModel(training_tensor, training_states, validation_tensor, validation_states)
-            predicted_results, evaluation, _ = machine_learning.test_model(test_tensor, test_states)
-            accuracy, precision, recall, f_score = machine_learning.evaluate_results(predicted_results, test_states)
-            if (accuracy, precision, recall, f_score) in self.__best_results:
-                tasks = self.__best_results.get((accuracy, precision, recall, f_score), default=None)
-                tasks.append(task)
-                self.__best_results.update({(accuracy, precision, recall, f_score): tasks})
-            else:
-                self.__best_results[(accuracy, precision, recall, f_score)] = [task]
-        for key, value in self.__best_results.items():
-            print("Key: ", key, " Value: ", value)
+            paths, test_paths, validation_number = self.__select_paths(task)
+            test_states, test_tensor, training_states, training_tensor, validation_states, validation_tensor = self.__extract_rhs_segment(
+                paths, test_paths, validation_number)
+            self.__best_results = TaskSelection.__create_and_evaluate_model(task, test_states, test_tensor, training_states, training_tensor,
+                                                                            validation_states, validation_tensor, self.__best_results)
 
+    def execute_task_selection(self):
+        previous_max = max(self.__best_results.items())
+        actual_tuple = max(self.__best_results.items())
+        while previous_max[KEY_TUPLE] <= actual_tuple[KEY_TUPLE]:
+            actual_tasks = actual_tuple[VALUE_TUPLE]
+            best_results = {}
+            for task in self.__tasks:
+                actual_tasks.append(task)
+                paths, test_paths, validation_number = self.__select_paths(actual_tasks)
+                test_states, test_tensor, training_states, training_tensor, validation_states, validation_tensor = self.__extract_rhs_segment(
+                    paths, test_paths, validation_number)
+                best_results = TaskSelection.__create_and_evaluate_model(actual_tasks, test_states, test_tensor, training_states, training_tensor,
+                                                                         validation_states, validation_tensor, best_results)
+            previous_max = deepcopy(actual_tuple)
+            actual_tuple = max(best_results.items())
+        return previous_max
+
+    def __select_paths(self, task):
+        paths = TaskManager.get_list_from_task(task, self.__file_manager.get_files_path())
+        paths = FileManager.filter_file(paths, min_dim=self.__minimum_samples + 1)
+        test_paths = TaskSelection.__get_test_paths(paths)
+        validation_number = int(np.ceil(len(paths) * 0.2))
+        return paths, test_paths, validation_number
 
     @staticmethod
     def __get_test_paths(paths):
@@ -55,4 +66,31 @@ class TaskSelection:
                 paths.remove(paths[i])
             i += 1
         return test_paths
+
+    def __extract_rhs_segment(self, paths, test_paths, validation_number):
+        training_tensor, training_states, _ = self.__feature_extraction.extract_rhs_known_state(
+            paths[0: (len(paths) - validation_number)])
+        validation_tensor, validation_states, _ = self.__feature_extraction.extract_rhs_known_state(
+            paths[(len(paths) - validation_number): len(paths)])
+        test_tensor, test_states, _ = self.__feature_extraction.extract_rhs_known_state(test_paths)
+        return test_states, test_tensor, training_states, training_tensor, validation_states, validation_tensor
+
+    @staticmethod
+    def __create_and_evaluate_model(task, test_states, test_tensor, training_states, training_tensor,
+                                    validation_states, validation_tensor, best_results):
+        machine_learning = MLModel(training_tensor, training_states, validation_tensor, validation_states)
+        predicted_results, evaluation, _ = machine_learning.test_model(test_tensor, test_states)
+        accuracy, precision, recall, f_score = machine_learning.evaluate_results(predicted_results, test_states)
+        best_results = TaskSelection.__fill_dictionary(best_results, accuracy, f_score, precision, recall, task)
+        return best_results
+
+    @staticmethod
+    def __fill_dictionary(best_results, accuracy, f_score, precision, recall, task):
+        if (accuracy, precision, recall, f_score) in best_results:
+            tasks = best_results.get((accuracy, precision, recall, f_score), None)
+            tasks.append(task)
+            best_results.update({(accuracy, precision, recall, f_score): tasks})
+        else:
+            best_results[(accuracy, precision, recall, f_score)] = [task]
+        return best_results
 
--- src/task_select_test.py
+++ src/task_select_test.py
@@ -3,13 +3,13 @@
 from Expreriment import TaskSelection
 
 SAMPLES_LEN = 50
-MINIMUM_SAMPLES = 2500
-VALIDATION_PATIENTS = 20
-TEST_PATIENTS = 1
+MINIMUM_SAMPLES = 2000
 
 
 def main():
-    task_selection = TaskSelection(SAMPLES_LEN, MINIMUM_SAMPLES, VALIDATION_PATIENTS, TEST_PATIENTS)
+    task_selection = TaskSelection(SAMPLES_LEN, MINIMUM_SAMPLES)
+    best_task = task_selection.execute_task_selection()
+    print("Best task combination (text dependent): ", best_task)
 
 
 if __name__ == '__main__':
