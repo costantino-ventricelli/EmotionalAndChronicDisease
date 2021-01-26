036cc243fb9707d1af98bc7a701254bf64bef93b
Costantino <costantino.ventricelli@gmail.com>
2021-01-26 17:52:17 +0100

    Try to fix and find TypeError in TaskManager.py

--- Expreriment/TaskSelection.py
+++ Expreriment/TaskSelection.py
@@ -61,6 +61,7 @@ class TaskSelection:
         # La selezione continuerà fino al deterioramento dei risultati, ovvero appena uno dei quatto paramentri si abbassa
         # la selezione viene interrotta.
         while previous_max[KEY_TUPLE] <= actual_tuple[KEY_TUPLE]:
+            print("Actual tuple: ", actual_tuple)
             # Seleziono la lista dei task appartenente al migliore dei risultati selezionato precedentemente.
             actual_tasks = actual_tuple[VALUE_TUPLE]
             # Creo il nuovo dizionario che permetterà di salvare i risultati
